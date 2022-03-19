657a1624188d9c725517f70a870d2752e8ad1a0e
JusupBusurmankulov <zusupbusurmankulov@gmail.com>
2022-03-19 12:33:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int [] array = {3, 4, 1, 2, -2, -7};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+        }
+
+
+}
